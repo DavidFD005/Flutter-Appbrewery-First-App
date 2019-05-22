88b3a1171ede8d506c53ca4b7956ad7c40bbb3cb
David Franquet <david@wearemobilefirst.com>
2019-05-22 11:13:48 +0200

    Finish first Flutter app

--- flutter_first/lib/main.dart
+++ flutter_first/lib/main.dart
@@ -1,4 +1,25 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(MaterialApp(home: Text('Hello World')));
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.red[100],
+        appBar: AppBar(
+          title: Text("We Are Flutter First"),
+          backgroundColor: Colors.red[900],
+        ),
+        body: Center(
+          child: Image(
+            image: AssetImage('assets/images/diamond.png'),
+          ),
+        ),
+      ),
+    );
+  }
+}
+
 
